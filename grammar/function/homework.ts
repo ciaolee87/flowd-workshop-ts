63b5f418d0a758a36a5b35018c92e5e5b5e0c182
CiaoLee <flowdeeep@gmail.com>
2022-03-16 15:21:23 +0900

    숙제(2)

--- grammar/function/homework.ts
+++ grammar/function/homework.ts
@@ -1,8 +1,27 @@
 // 3-16 :
 // 1.
 
-[1,2,3,4].map();
-[1,2,3,4].sort();
-[1,2,3,4].filter();
-[1,2,3,4].findIndex();
-[1,2,3,4].forEach();
+
+
+[1, 2, 3, 4].map();
+
+// 똑같은걸 만들어봐
+const myMap = <T, R>(array: T[], callback: (value: T, index: number, array: T[]) => R): R[] => {
+
+    return [];
+
+}
+
+// 짝수번째 맵
+const evenMap = <T, R>(array: T[], callback: (value: T, index: number, array: T[]) => R): R[] => {
+
+    return [];
+
+}
+
+
+[1, 2, 3, 4].sort();
+[1, 2, 3, 4].filter();
+[1, 2, 3, 4].findIndex();
+[1, 2, 3, 4].forEach();
+
